3c0e10075eb78cab18bc26767be6599d83f7382d
vendulaprochazkova <vendula.prochazkova@infis.cz>
2025-03-11 09:14:59 +0100

    Add files via upload

---
+++ k.py
@@ -0,0 +1,14 @@
+#prochazkova
+#priklad pretypovani na cislo
+cislo = int(input())
+#priklad pretypovani na boolean
+pravdaNepravda = bool(input())
+
+if cislo == 15:
+    print()
+elif (cislo==16):
+    print()
+
+# git prikaz 1
+# git prikaz 2
+# git prikaz 3
